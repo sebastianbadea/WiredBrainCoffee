c529f00c2cf855e68800574dd0045c15670c71d6
Sebastian Badea <sebastian.badea@gmail.com>
2019-06-29 12:19:33 +0100

    display the coffeeshops for a search criteria

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrainCoffee.ShopInfoTool
@@ -7,7 +8,7 @@ namespace WiredBrainCoffee.ShopInfoTool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Write 'help' to list the coffee shops or 'quit' to leave the program! MTF");
+            Console.WriteLine("Write 'help' to list the coffee shops or 'quit' to leave the program! Then write the name of the location");
 
             var provider = new CoffeeShopDataProvider();
 
@@ -26,6 +27,34 @@ namespace WiredBrainCoffee.ShopInfoTool
                     foreach (var shop in coffeeShops)
                     {
                         Console.WriteLine($"> {shop.Location}");
+                    }
+                }
+                else
+                {
+                    var availableShops = coffeeShops.Where(cs => cs.Location.ToLower().StartsWith(line.ToLower()));
+
+                    var availableShopsCount = availableShops.Count();
+
+                    if (availableShopsCount == 0)
+                    {
+                        Console.WriteLine($"> No coffee shops found for {line}");
+                    }
+                    else
+                    {
+                        if (availableShopsCount > 1)
+                        {
+                            Console.WriteLine($"> Multiple coffees shops available for the search criteria");
+
+                            foreach (var availableShop in availableShops)
+                            {
+                                Console.WriteLine(availableShop.Location);
+                            }
+                        }
+                        else
+                        {
+                            var availableShop = availableShops.Single();
+                            Console.WriteLine($"> {availableShop.Location} - {availableShop.BeansInStockInKg}");
+                        }
                     }
                 }
             }
