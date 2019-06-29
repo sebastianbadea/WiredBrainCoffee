b2f49a54fa8321e12ebc3277c87784a940516373
Sebastian Badea <sebastian.badea@gmail.com>
2019-06-29 10:59:39 +0100

    add data access project

---
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 10};
+            yield return new CoffeeShop { Location = "Purcareni", BeansInStockInKg = 50 };
+            yield return new CoffeeShop { Location = "Tatarasti de Sus", BeansInStockInKg = 100 };
+        }
+    }
+}
